9c3524c7aa34aeaf4a2b61a335547b9840f1e33e
Iván <a01705083@tec.mx>
2023-08-10 13:28:30 -0600

    Javascript

---
+++ Peliculas/intro.js
@@ -0,0 +1,69 @@
+// variables, constantes, consola (log, info, warn, error, assert)
+
+// Declaramos variables con let
+let pelicula_del_momento = "Barbie";
+
+//Oprimimos en la consola con console.log()
+console.log(pelicula_del_momento);
+
+//Declaramos constantes con const
+const precio = 55;
+
+console.log(precio);
+
+console.error("Este es un mensaje de error");
+
+console.warn("Esta es una advertencia");
+
+console.info("Esto es información");
+
+//assert continua la ejecución si el valor es verdadero
+console.assert(precio === 55);
+
+// Alcance de las variables
+
+if(precio > 50) {
+    let respuesta = "Muy caro"
+    console.log(respuesta);
+} else {
+    let respuesta = "Buen precio";
+    console.log(respuesta);
+}
+
+if(precio > 50) {
+    var respuesta2 = "Muy caro"
+} else {
+    var respuesta2 = "Buen precio";
+}
+//Respuesta sigue existiendo porque var tiene alcance de función
+console.log(respuesta2);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//La siguiente línea tiene error porque i tiene alcance de ámbito
+//console.log(i);
+
+//alert, prompt, confirm
+alert("Hola mundo!");
+
+const is_hungry = confirm("¿Tienes hambre?");
+console.log(is_hungry);
+
+const nombre = prompt("¿Cómo te llamas?");
+console.log("Hola " + nombre);
+
+//funciones tradicionales
+
+function vamonos() {
+    console.log("¡Vámonos, ya se acabó la clase");
+}
+
+vamonos();
+
+//funciones modernas
+
+
+
+
