eddc77eda42ac7d51e75d4d1de67c1a3b980b4a4
Iván <a01705083@tec.mx>
2023-08-15 12:01:09 -0600

    Guardar cambios de peliculas

--- Peliculas/intro.js
+++ Peliculas/intro.js
@@ -1,9 +1,9 @@
 // variables, constantes, consola (log, info, warn, error, assert)
 
-// Declaramos variables con let
-let pelicula_del_momento = "Barbie";
+//Declaramos variables con let
+let pelicula_del_momento =  "Barbie";
 
-//Oprimimos en la consola con console.log()
+//Imprimimos en la consola con console.log()
 console.log(pelicula_del_momento);
 
 //Declaramos constantes con const
@@ -17,26 +17,19 @@ console.warn("Esta es una advertencia");
 
 console.info("Esto es información");
 
-//assert continua la ejecución si el valor es verdadero
+//assert continúa la ejecución si el valor es verdadero
 console.assert(precio === 55);
+
 
 // Alcance de las variables
 
 if(precio > 50) {
-    let respuesta = "Muy caro"
-    console.log(respuesta);
+    var respuesta = "Muy caro";
 } else {
-    let respuesta = "Buen precio";
-    console.log(respuesta);
+    var respuesta = "Buen precio";
 }
-
-if(precio > 50) {
-    var respuesta2 = "Muy caro"
-} else {
-    var respuesta2 = "Buen precio";
-}
-//Respuesta sigue existiendo porque var tiene alcance de función
-console.log(respuesta2);
+//respuesta sigue existiendo porque var tiene alcance de función
+console.log(respuesta);
 
 for (let i = 1; i <= 10; i++) {
     console.log(i);
@@ -45,38 +38,41 @@ for (let i = 1; i <= 10; i++) {
 //La siguiente línea tiene error porque i tiene alcance de ámbito
 //console.log(i);
 
-//alert, prompt, confirm
-alert("Hola mundo!");
+
+// alert, prompt, confirm
+/*
+alert("Hola mundo!")
 
 const is_hungry = confirm("¿Tienes hambre?");
 console.log(is_hungry);
 
 const nombre = prompt("¿Cómo te llamas?");
 console.log("Hola " + nombre);
+*/
 
-//funciones tradicionales
+// funciones tradicionales
 
 function vamonos() {
-    console.log("¡Vámonos, ya se acabó la clase");
+    console.log("¡Vámonos, ya se acabó la clase!");
 }
 
 vamonos();
 
-//funciones modernas
 
+// funciones modernas
 const funcion_anonima = () => {
     console.log("Esto es una función anónima");
 }
 
 funcion_anonima();
 
-//html dinámico con eventos
+// html dinámico con eventos
 const oppenheimer = document.getElementById("Oppenheimer");
 
 console.log(oppenheimer);
 
 const despliega_rating = () => {
-    const rating = "10/10 ¡Excelente!"
+    const rating = "10/10 ¡Excelente!";
     oppenheimer.innerHTML = rating;
     oppenheimer.onclick = despliega_nombre;
 }
@@ -92,11 +88,12 @@ oppenheimer.onclick = () => {
     despliega_rating();
 }
 
+
 // arreglos
 
 const arreglo = ["Elemento"];
 
-arreglo.push ("Otro elemento");
+arreglo.push("Otro elemento");
 
 arreglo[10] = "Uno más";
 
@@ -120,8 +117,8 @@ for(let valor of arreglo) {
 //objetos (json: javascript object notation)
 const objeto = {
     atributo_1: "valor_1",
-    atributo_2: "valor_2",
-    atributo_3: "valor_3",
+    atributo_2: "valor_2", 
+    atributo_3: "valor_3"
 }
 
 console.log(objeto);
@@ -133,9 +130,82 @@ for(let posicion in objeto) {
 
 function ejercicio_1() {
     //Ejercicio del laboratorio
-    //....
+    //...
 
     document.getElementById("ejercicio_1").innerHTML = "Respuesta del ejercicio 1";
 }
 
 ejercicio_1();
+
+
+document.getElementById("favorita").onkeyup = () => {
+    
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    
+    document.getElementById("favorita").style.color = `rgb(${red}, ${green}, ${blue})`;
+}
+
+const peliculas = [
+    {
+        nombre: "Titanic",
+        imagen: "https://i.etsystatic.com/27725708/r/il/3989d8/2912524873/il_fullxfull.2912524873_6vc1.jpg",
+        sinopsis: "Historia de amor entre Jack y Rose en el trasatlántico más grande de la época"
+    }, 
+    {
+        nombre: "Oppenheimer",
+        imagen: "https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_.jpg",
+        sinopsis: "Historia de la bomba atómica"
+    }, 
+    {
+        nombre: "Barbie",
+        imagen: "https://www.instyle.com/thmb/vZCEkHB1nBMIes2tcKTUAMP0zf0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/BarbiePosterEmbed-de7c886812184414977730e920d77a65.jpg",
+        sinopsis: "La muñeca más famosa de la historia"
+    }, 
+    {
+        nombre: "TMNT",
+        imagen: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/5d830b134127975.61ce89a78d10c.jpg",
+        sinopsis: "Las aventuras flipantes de las tortugas verdes en Nueva York"
+    },
+];
+
+function genera_posters() {
+
+    let html = `<div class="columns">`;
+
+    for(let pelicula of peliculas) {
+        html += 
+            `<div class="column">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="${pelicula.imagen}" alt="${pelicula.nombre}">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="${pelicula.imagen}" alt="${pelicula.nombre}">
+                            </figure>
+                            </div>
+                            <div class="media-content">
+                            <p class="title is-4">${pelicula.nombre}</p>
+                            </div>
+                        </div>
+                    
+                        <div class="content">
+                            ${pelicula.sinopsis}
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    }
+
+    html += `</div>`;
+
+    document.getElementById("posters").innerHTML = html;
+}
+
+genera_posters();
