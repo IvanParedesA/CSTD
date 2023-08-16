9f93cba6b7d9b5eed8e2f78b72183d1879f74cdf
Iván <a01705083@tec.mx>
2023-08-16 17:46:15 -0600

    Ejercicio 2 Lab4

--- Laboratorio 4/intro.js
+++ Laboratorio 4/intro.js
@@ -1,5 +1,8 @@
+//==============
 //Ejercicio 1
-document.write("Ejercicio 1: <br>")
+//==============
+
+document.write("Ejercicio 1: <br><br>")
 
 //Prompt para que el usuario ingrese un número
 const numero = Number(prompt("Inserte un número: "));
@@ -8,7 +11,7 @@ const numero = Number(prompt("Inserte un número: "));
 let tabla = `<table">`;
 
 //Ciclo para introducir filas y celdas a la tabla
-for( let i = 1; i < numero + 1; i++){
+for(let i = 1; i < numero + 1; i++){
     tabla +=
         `
         <tr>
@@ -26,3 +29,37 @@ tabla += `</table>`;
 //Se escribe la tabla en el documento html
 document.write(tabla);
 
+//==============
+//Ejercicio 2
+//==============
+
+document.write("<br> Ejercicio 2: <br><br>")
+
+//Creamos variables para los números aleatorios
+let random1 = Math.floor(Math.random()*100);
+let random2 = Math.floor(Math.random()*100);
+
+//Definimos la variable de suma
+let suma = random1 + random2;
+
+//Variable con el objeto Date que guarda el tiempo en que se empezó la operación
+let tiempoInicio = new Date();
+
+//Prompt con la suma para que la responda el usuario
+let respuesta = Number(prompt("Resuelve: " + random1 + " + " + random2));
+
+//Variable con el objeto Date que guarda el tiempo en que se finalizó la operación
+let tiempoFinal = new Date();
+
+//Calculamos el tiempo que transcurrió entre el inicio y el final
+let tiempoDiferencia = tiempoFinal - tiempoInicio;
+
+//Transformamos milisegundos a segundos
+let diferenciaSegundos = tiempoDiferencia / 1000;
+
+//Condicionales
+if(respuesta == suma) {
+    document.write("¡Haz logrado hacer la suma en " + diferenciaSegundos + " segundos! <br>");
+} else {
+    document.write("No lograste resolver la suma y tardaste " + diferenciaSegundos + " segundos. :( <br>");
+}
