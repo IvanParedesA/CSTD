b392d2b33cf7ea35b9bfc43cccec0682b2768514
Iván <a01705083@tec.mx>
2023-08-16 18:23:15 -0600

    Ejercicio 3 Lab4

--- Laboratorio 4/intro.js
+++ Laboratorio 4/intro.js
@@ -63,3 +63,49 @@ if(respuesta == suma) {
 } else {
     document.write("No lograste resolver la suma y tardaste " + diferenciaSegundos + " segundos. :( <br>");
 }
+
+//==============
+//Ejercicio 3
+//==============
+
+document.write("<br> Ejercicio 3: <br><br>")
+
+//Creamos el arreglo con números y lo ordenamos con .sort()
+let arreglo = [2, 5, -8, 0, 6, 3, -7, 3, 0, 4, 2, 9, -4, 8, 6, 0];
+
+//Ordenamos el arreglo con la función .sort()
+arreglo.sort();
+
+//Mostramos el arreglo para verificar el funcionamiento de la función
+document.write("Arreglo: ");
+document.write("[" + arreglo + "] <br><br>");
+
+//Definimos la función
+function contador(inputArreglo) {
+
+    //Definimos los contadores para la función
+    let negativos = 0;
+    let ceros = 0;
+    let positivos = 0;
+
+    //Ciclo para encontrar los números
+    for(let i = 0; i < inputArreglo.length; i++) {
+        if(inputArreglo[i] < 0) {
+            negativos ++;
+        }
+        else if(inputArreglo[i] === 0) {
+            ceros ++;
+        }
+        else if(inputArreglo[i] > 0){
+            positivos ++;
+        }
+    }
+
+    //Enviamos la respuesta
+    document.write("La cantidad de números negativos es de: " + negativos + "<br>");
+    document.write("La cantidad de ceros en el arreglo es de: " + ceros + "<br>");
+    document.write("La cantidad de números mayores a cero es de: " + positivos + "<br>");
+}
+
+//Llamamos a la función
+console.log(contador(arreglo));
