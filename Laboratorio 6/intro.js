6a42114c503b6471728aa54f6b1af71233812e24
Iván <a01705083@tec.mx>
2023-08-29 18:27:56 -0600

    Funcionamiento despliegue de cuenta

--- Laboratorio 6/intro.js
+++ Laboratorio 6/intro.js
@@ -4,21 +4,25 @@ let precioCuboRubik = 382;
 let precioFunkoPop = 447;
 let precioHotWheels = 99;
 
-console.log(precioCuboRubik);
-
 //Función para calcular la cuenta
 function calcularCuenta() {
     let cuentaFinal = 0;
 
-    let cantCubo = document.getElementById("cantidadCuboRubik").value;
-    let cantFunko = document.getElementById("cantidadFunkoPop").value;
-    let cantHotWheels = document.getElementById("cantidadHotWheels").value;
+    let cantCubo = document.getElementById("cantidadCuboRubik").value || 0;
+
+    let cantFunko = document.getElementById("cantidadFunkoPop").value || 0;
+
+    let cantHotWheels = document.getElementById("cantidadHotWheels").value || 0;
 
     cuentaFinal += (cantCubo * precioCuboRubik);
     cuentaFinal += (cantFunko * precioFunkoPop);
     cuentaFinal += (cantHotWheels * precioHotWheels);
 
-    console.log(cuentaFinal);
+    document.write("Desglose de artículos:" + "<br><br>")
+    document.write("Has elegido " + cantCubo + " cubos Rubik" + "<br>");
+    document.write("Has elegido " + cantFunko + " Funkos" + "<br>");
+    document.write("Has elegido " + cantHotWheels + " HotWheels" + "<br>");
+    document.write("<br>" + "Tu cuenta final es de: $" + cuentaFinal);
 };
 
 document.getElementById("botonCuenta").onclick = calcularCuenta;
