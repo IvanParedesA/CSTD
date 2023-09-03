1e15f9be34c73842018ec17c8698a0a0d65c1cad
Iván <a01705083@tec.mx>
2023-09-02 18:52:24 -0600

    Agregar Ruta Raíz

--- app.js
+++ app.js
@@ -96,6 +96,100 @@ app.post('/new', (request, response, next) => {
     response.redirect('/')
 });
 
+app.get('/', (request, response, next) => {
+
+    let html = 
+    `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>Coleccionables</title>
+    </head>
+    
+    <body>
+        <!--Encabezado-->
+        <header>
+            <div class="bg-gradient-to-r from-cyan-500 to-lime-500 m-6 p-2 rounded-xl p-3">
+                <h1 class="text-6xl font-sans text-black text-center" id="Titulo" name="Titulo">Tienda de coleccionables</h1>
+            </div>
+        </header>
+    
+        <!--Parte central de la página-->
+        <main>
+    
+            <!--Columnas para los productos-->
+            <div class="grid grid-cols-3 gap-3 mx-auto flex items-center">`;
+
+                for(let coleccionable of coleccionables) {
+                    html += 
+                    `
+                    <div>
+                        <div class="bg-gray-600 m-6 p-4 rounded-xl text-center">
+                            <div class="container bg-gray-300 mx-auto p-4 rounded-xl">
+                                <p class="text-2xl font-sans text-black">${coleccionable.nombre}</p>
+                                <br>
+                                <img class="mx-auto" src="${coleccionable.imagen}" alt="${coleccionable.nombre}" width="300px">
+                                <br>
+                                <p class="text-2xl font-sans text-black">Precio: $${coleccionable.valor}</p>
+                                <br>
+                                <p class="text-1xl font-sans text-black"> Cantidad:
+                                    <input type="number" id="cantidad${coleccionable.nombre}" name="cantidad${coleccionable.nombre}" placeholder="0" min="0" max="100" class="w-16">
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                }
+
+            html += 
+            `
+            </div>
+    
+            <!--Botón para calcular cuenta total-->
+            <div class="flex justify-center">
+                <button id="botonCuenta" class="bg-red-500 hover:bg-red-600 w-48 h-12 rounded-full">
+                    Calcular Cuenta
+                </button>
+            </div>
+            <br>
+    
+            <!--Mensaje de ayuda oculto-->
+            <div class="flex justify-center mx-64">
+                <p id="ayuda" style="visibility:hidden"> 
+                    (Este es un mensaje de ayuda oculto) ¡Introduce texto y verás que el color cambia!
+                </p>
+            </div>
+            
+            <!--Entrada de texto que cambiar de color-->
+            <div class="bg-gray-600 m-6 p-4 rounded-xl flex justify-center mx-64">
+                <p>
+                    <label for="sugerencia">¿Qué otro colleccionable te gustaría ver en nuestra tienda? </label>
+                    <input id="sugerencia" type="text" placeholder="Tarjetas Pokemon">
+                </p>
+            </div>
+        </main>
+    
+        <!--Pie de página-->
+        <footer>
+            <div class="bg-gray-900 m-6 p-2 rounded-xl p-3">
+                <p class="text-center text-1xl font-sans text-white">Editor html utilizado en esta página web: Visual Studio Code</p>
+                <p class="text-center text-1xl font-sans text-white">Enlace al sitio del editor: <a href="https://code.visualstudio.com/">https://code.visualstudio.com/</a></p>
+                <br>
+                <p class="text-center text-1xl font-sans text-white">Framework de estilo utilizado: Tailwind CSS</p>
+                <p class="text-center text-1xl font-sans text-white">Enlace al sitio del framework de estilo: https://tailwindcss.com/</p>
+            </div>
+        </footer>
+    
+    </body>
+    </html>
+    `;
+    
+});
+
 app.use((request, response, next) => {
     console.log('Otro middleware!');
     response.send('¡Hola mundo!'); //Manda la respuesta
