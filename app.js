76b12440a67e53c75e639ec2c7c69cbe785a4a1c
Iván <a01705083@tec.mx>
2023-09-02 18:39:17 -0600

    Instalar body parser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
 //Middleware
 app.use((request, response, next) => {
     console.log('Middleware!');
