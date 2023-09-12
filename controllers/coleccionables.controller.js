2c23e0060505b34496ad61d83cd7852b898c13ce
Iván <a01705083@tec.mx>
2023-09-11 21:44:01 -0600

    tiempo transcurrido entre la última interacción con el sitio y la interacción actual

--- controllers/coleccionables.controller.js
+++ controllers/coleccionables.controller.js
@@ -22,8 +22,21 @@ exports.post_add = (request, response, next) => {
 
 exports.get_list = (request, response, next) => {
 
+    //Extraer la fecha de último acceso almacenada en una cookie
+    const ultimo_acceso = new Date(request.get('Cookie').split('=')[1]);
+
+    //Imprime en la consola la marca de tiempo del último acceso
+    console.log(ultimo_acceso.getTime());
+
+    //Se calcula el tiempo transcurrido desde el último acceso en segundos.
+    const tiempo_transcurrido = (new Date().getTime() - ultimo_acceso.getTime()) / 1000;
+
+    //Imprime en la consola el tiempo transcurrido desde el último acceso en segundos.
+    console.log(tiempo_transcurrido);
+
     response.render('coleccionables/list.ejs', {
-        coleccionables: Coleccionable.fetchAll()
+        coleccionables: Coleccionable.fetchAll(),
+        tiempo_transcurrido: tiempo_transcurrido
     });
 }
 
