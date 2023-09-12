c24efd94bc2c5504899be3c9e31d382461e5ede4
Iván <a01705083@tec.mx>
2023-09-11 22:00:04 -0600

    Instalar y configurar express-session

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.set('views', 'views');
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
+const session = require('express-session');
+
+app.use(session({
+
+    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
