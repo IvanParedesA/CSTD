a5e9c9015cd37629422b11a04d3effbe8f023643
Iván <a01705083@tec.mx>
2023-09-13 21:18:32 -0600

    cifrar passwords

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -1,4 +1,5 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
 
 module.exports = class Usuario {
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
@@ -10,8 +11,11 @@ module.exports = class Usuario {
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        return db.execute(
-            'INSERT INTO usuarios(username, password, nombre) VALUES (?, ?, ?)', 
-            [this.username, this.password, this.nombre]);
+        return bcrypt.hash(this.password, 12)
+        .then((password_cifrado) => {
+            return db.execute(
+                'INSERT INTO usuarios(username, password, nombre) VALUES (?, ?, ?)', 
+                [this.username, password_cifrado, this.nombre]);
+        }).catch((error) => {console.log(error)}); 
     }
 }
